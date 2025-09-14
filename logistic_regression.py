279d12b133535ffd3bd2b618f76e81639f593572
Raj Kumar <raj@Rajs-MacBook-Air.local>
2025-09-14 14:02:24 +0530

    Added logistic regression

---
+++ logistic_regression.py
@@ -0,0 +1,57 @@
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+X, y = make_classification(
+    n_samples=1000,
+    n_features=2,
+    n_informative=2,
+    n_redundant=0,
+    n_classes=2,
+    random_state=42
+)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_state=42)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+X_train = torch.tensor(X_train, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32).view(-1,1)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_test = torch.tensor(y_test, dtype=torch.float32).view(-1,1) 
+
+
+class LogisticRegression(nn.Module):
+    def __init__(self, n_features):
+        super(LogisticRegression, self).__init__()
+        self.linear = nn.Linear(n_features, 1)
+    
+    def forward(self,x):
+        return torch.sigmoid(self.linear(x))
+model = LogisticRegression(n_features=2)
+
+criterion = nn.BCELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+model = LogisticRegression(n_features=2)
+
+epochs = 100
+for epoch in range(epochs):
+    y_pred = model(X_train)
+    loss = criterion(y_pred, y_train)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    if (epoch+1)%10 == 0:
+        print(f"Epoch [{epoch+1}/{epochs}] Loss: {loss.item():.4f}")
+
+with torch.no_grad():
+    y_pred_test = model(X_test)
+    y_pred_cls = (y_pred_test >=0.5).float()
+    accuracy = (y_pred_cls.eq(y_test).sum())/ y_test.shape[0]
+    print(f"Test Accuracy: {accuracy:.4f}")
