55133ee2ecc470c4f2c3eec6854271fee9dd0864
Raj Kumar <raj6713631@gmail.com>
2025-09-21 17:14:29 +0530

    loss_functions updated with Categorical cross entropy

--- loss_functions.py
+++ loss_functions.py
@@ -1,7 +1,68 @@
-# import torch
-# import torch.nn as nn
 import torch
 import torch.nn as nn
+# import torch
+# import torch.nn as nn
+
+# class CategoricalCrossEntropy(nn.Module):
+#     def __init__(self):
+#         super(CategoricalCrossEntropy, self).__init__()
+
+#     def forward(self, y_pred, y_true):
+#         """
+#         y_pred: logits (batch_size, num_classes)   # raw outputs from model
+#         y_true: class indices (batch_size,)        # true class labels (not one-hot)
+#         """
+#         # Apply log softmax to logits
+#         log_probs = torch.log_softmax(y_pred, dim=1)
+
+#         # Gather the log-probability of the correct class for each sample
+#         loss = -log_probs[range(y_true.shape[0]), y_true]
+
+#         # Return mean loss
+#         return loss.mean()
+
+class CategoricalCrossEntropy(nn.Module):
+    def __init__(self):
+        super(CategoricalCrossEntropy, self).__init__()
+    
+    def forward(self, y_pred, y_true):
+        log_probs = torch.log_softmax(y_pred, dim=1)
+        loss = -log_probs[range(y_true.shape[0]), y_true]
+        return loss.mean()
+
+y_pred = torch.tensor([[2.0,1.0,0.1,-1.2],
+                       [0.5,2.3, -0.5,0.0],
+                       [1.2, -0.3,2.1, 0.8]], requires_grad=True)
+y_true = torch.tensor([0,1,2])
+criterion = CategoricalCrossEntropy()
+loss = criterion(y_pred, y_true)
+print("Custom CCE Loss:", loss.item())
+exit()
+
+
+def binary_cross_entropy(y_pred, y_true):
+    eps = 1e-8
+    y_pred = torch.clamp(y_pred, eps,1-eps)
+    loss = -(y_true*torch.log(y_pred)+ (1-y_true)*torch.log(1-y_pred))
+    return loss.mean()
+
+y_true = torch.tensor([1.,0., 1.,1.])
+y_logits = torch.tensor([2.0, -1.0,1.5,-2.0])
+y_pred = torch.sigmoid(y_logits)
+loss_custom = binary_cross_entropy(y_pred, y_true)
+bce_loss = nn.BCELoss()
+loss_builtin = bce_loss(y_pred, y_true)
+bce_logits_loss = nn.BCEWithLogitsLoss()
+loss_stable = bce_logits_loss(y_logits, y_true)
+print("Custom BCE Loss: ", loss_custom.item())
+print("nn.BCELoss: ", loss_builtin.item())
+print("nn.BCELWithLogitsLoss(recommended): ", loss_stable.item())
+exit()
+
+
+
+
+
 
 class MSELoss(nn.Module):
     def __init__(self):
